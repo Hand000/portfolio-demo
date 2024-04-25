f58cb0260d35b80d5ae97dbcf8065bfa1a9b8eea
Paul Hands <paul@192.168.1.101>
2024-04-25 22:05:45 +0100

    Fiddled with some updates to the home page, added all content.

--- src/components/collage.tsx
+++ src/components/collage.tsx
@@ -5,6 +5,7 @@ import '../style/collage.css';
 interface CollageProps {
     background: string;
     title?: string;
+    subtitle?: string;
     caption: string;
     left?: boolean;
 }
@@ -21,9 +22,11 @@ export default class Collage extends React.Component<CollageProps, CollageState>
     render() {
         return (
             <div className='collage-container'>
-                <img className='collage-image' src={this.props.background}/>
-                <div className={`collage-caption ${this.props.left ? 'collage-caption-left' : 'collage-caption-right'}`}>
+                <img className={`collage-image ${this.props.left ? 'collage-image-right' : ''}`} src={this.props.background}/>
+                <img className='collage-scale' src={this.props.background}/>
+                <div className={`collage-caption ${this.props.left ? 'collage-caption-left' : ''}`}>
                     { this.props.title ? <div className='collage-title'>{this.props.title}</div> : null }
+                    { this.props.subtitle ? <div className='collage-subtitle'>{this.props.subtitle}</div> : null }
                     <div className='collage-content'>{this.props.caption}</div>
                 </div>
             </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,6 +8,7 @@ import '../style/home.css';
 import Collage from '../components/collage';
 
 import collage1 from '../img/dukes/b.jpg';
+import collage2 from '../img/outdoor/z27.jpg';
 
 interface HomeProps {
 
@@ -24,17 +25,58 @@ export default class Home extends React.Component<HomeProps, HomeState> {
 
     render() {
         return (
-            <div className='home-slideshow'>
-                <Slideshow 
-                    title='Interiors | Exteriors | Flower Arrangements'
-                    slides={[slide1, slide2]}/>
+            <div id='home-page'>
+                <div className='home-slideshow'>
+                    <Slideshow 
+                        title='Interiors | Exteriors | Flower Arrangements'
+                        slides={[slide1, slide2]}/>
+                    </div>
                 <div id='home-services'>
-                    <div id='home-services-title'>
+                    <div className='home-subtitle'>
                         Services
                     </div>
-                    <Collage background={collage1} caption='poopoo'/>
+                    <Collage background={collage1} title='interior' subtitle='Refresh, Restyle, Reinvent.' caption='Friendly consultation, mood boards, visuals, procurement, installation.'/>
+                    <Collage background={collage2} title='exterior' subtitle='Refresh, Restyle, Reinvent.' caption='Artificial or real plants. Floral walls & pergola’s, furniture, planters, painted & printed murals, visuals & layout. .' left={true} />
+                </div>
+                <div id='home-testimonials'>
+                    <div className='home-subtitle'>
+                        Testimonials
+                    </div>
+                    <div>
+                        <div className='testimonial-block'>
+                            <p className='testimonial-quote'>
+                            “John worked for me over many years on different projects. He is very creative and able to work within a budget. Friendly and a pleasure to work with.”
+                            </p>
+                            <div className='testimonial-source'> — Nigel Martin Smith, Essential Nightclub / Queer bar.  </div>
+                        </div>
+                        <div className='testimonial-block testimonial-block-r'>
+                            <p className='testimonial-quote'>
+                            After working with John on a number of previous hospitality interiors, when it came to selecting a designer to help realise our vision for an enormous Peaky Blinders themed bar and restaurant, in the heart of Manchester City Centre, I knew immediately he was the right person for the project. With his outstanding attention to detail, professional service and the ability to work wonders with a relatively limited budget, John delivered a unique look that was beyond expectations. I would recommend John to anyone looking for a truly personalised service and look forward to working with him again on future projects, his creativity is invaluable!
+                            </p>
+                            <div className='testimonial-source'>
+                            — Paul Wilson / GM Peaky Blinders Bar Manchester
+                            </div>
+                        </div>
+                        <div className='testimonial-block'>
+                            <p className='testimonial-quote'>
+                            The bar looks amazing and has its own identity, which I love. Everywhere you look there is something new to see.  
+                            </p>
+                            <div className='testimonial-source'>
+                            — Carly Sinclair /Director/ Tank Bar 
+                            </div>
+                        </div>
+                        <div className='testimonial-block testimonial-block-r'>
+                            <p className='testimonial-quote'>
+                            From beginning to the end, we can not thank you enough, you’re ideas and professionalism are simply first class! please don’t be a stranger we loved having you around. You are now officially a member of the Jam Works Family.
+                            </p>
+                            <div className='testimonial-source'>
+                            — Paula Mackinnon /Owner/ The Jam Works
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            
         );
     }
 }
